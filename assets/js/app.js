ea0cb4375b1e952345cbae70417af729e89d56d5
ManishSharma-17 <manishsincere17@gmail.com>
2023-08-13 23:13:00 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,47 @@
+function makeBubble() {
+    let bubble = "";
+    for (let i = 0; i <= 500; i++) {
+        let run = Math.floor(Math.random() * 10);
+        bubble += `<div class="bubble">${run}</div>`;
+    }
+    document.querySelector("#panel>section").innerHTML = bubble;
+};
+makeBubble()
+
+let time = 60;
+function runtimer() {
+    let timeslap = setInterval(() => {
+        if (time > 0) {
+            time--;
+            document.querySelector("#timerValue").innerHTML = time;
+        } else {
+            clearInterval(timeslap);
+            document.querySelector("#panel>section").innerHTML = `<h1 class="text-center">Game over🤚</h1>`;
+        }
+    }, 1000)
+};
+runtimer();
+
+let hitrun;
+function getNewHit() {
+    hitrun = Math.floor(Math.random() * 10);
+    document.querySelector("#hitVal").innerHTML = hitrun;
+}
+getNewHit()
+
+let score = 0;
+function increaseScore() {
+    score += 10;
+    document.querySelector("#scoreVal").textContent = score;
+}
+
+document.querySelector("#panel>section").addEventListener("click", function (e) {
+    if (e.target.className === "bubble") {
+        let clickedBubble = Number(e.target.textContent);
+        if (clickedBubble === hitrun) {
+            increaseScore();
+            makeBubble();
+            getNewHit();
+        }
+    }
+})
