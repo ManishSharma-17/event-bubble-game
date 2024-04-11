b0c483492c93ab6827bbfeb57318b04880a10116
ManishSharma-17 <manishsincere17@gmail.com>
2024-04-11 16:54:26 +0530

    adding restart functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,60 +1,72 @@
 let correct = document.querySelector("#correct");
 let wrong = document.querySelector("#wrong");
-
 function correctSound() {
-    correct.play()
+  correct.play();
 }
 function wrongSound() {
-    wrong.play()
+  wrong.play();
 }
-
 function makeBubble() {
-    let bubble = "";
-    for (let i = 0; i <= 500; i++) {
-        let run = Math.floor(Math.random() * 10);
-        bubble += `<div class="bubble">${run}</div>`;
-    }
-    document.querySelector("#panel>section").innerHTML = bubble;
-};
-makeBubble()
-
-let time = 30;
+  let bubble = "";
+  for (let i = 0; i <= 500; i++) {
+    let run = Math.floor(Math.random() * 10);
+    bubble += `<div class="bubble">${run}</div>`;
+  }
+  document.querySelector("#panel>section").innerHTML = bubble;
+}
+makeBubble();
+let time = 5;
+const bubbleContainer = document.querySelector("#panel>section");
+document.querySelector("#timerValue").innerHTML = time;
 function runtimer() {
-    let timeslap = setInterval(() => {
-        if (time > 0) {
-            time--;
-            document.querySelector("#timerValue").innerHTML = time;
-        } else {
-            clearInterval(timeslap);
-            document.querySelector("#panel>section").innerHTML = `<h1 class="text-center">Game over🤚</h1>`;
-        }
-    }, 1000)
-};
+  let timeslap = setInterval(() => {
+    if (time > 0) {
+      time--;
+      document.querySelector("#timerValue").innerHTML = time;
+    } else {
+      clearInterval(timeslap);
+      bubbleContainer.innerHTML = `<h1 class="text-center">Game over🤚</h1>`;
+      const restartBtn = document.createElement("h2");
+      restartBtn.setAttribute("class", "restart");
+      restartBtn.innerText = "Re-start";
+      bubbleContainer.appendChild(restartBtn);
+      restartBtn.addEventListener("click", restartGame);
+    }
+  }, 1000);
+}
 runtimer();
-
+function restartGame() {
+  time = 5;
+  document.querySelector("#timerValue").innerHTML = time;
+  score = 0;
+  document.querySelector("#scoreVal").textContent = score;
+  makeBubble();
+  getNewHit();
+  runtimer();
+}
 let hitrun;
 function getNewHit() {
-    hitrun = Math.floor(Math.random() * 10);
-    document.querySelector("#hitVal").innerHTML = hitrun;
+  hitrun = Math.floor(Math.random() * 10);
+  document.querySelector("#hitVal").innerHTML = hitrun;
 }
-getNewHit()
-
+getNewHit();
 let score = 0;
 function increaseScore() {
-    score += 10;
-    document.querySelector("#scoreVal").textContent = score;
+  score += 10;
+  document.querySelector("#scoreVal").textContent = score;
 }
-
-document.querySelector("#panel>section").addEventListener("click", function (e) {
+document
+  .querySelector("#panel>section")
+  .addEventListener("click", function (e) {
     if (e.target.className === "bubble") {
-        let clickedBubble = Number(e.target.textContent);
-        if (clickedBubble === hitrun) {
-            correctSound();
-            increaseScore();
-            makeBubble();
-            getNewHit();
-        } else {
-            wrongSound()
-        }
+      let clickedBubble = Number(e.target.textContent);
+      if (clickedBubble === hitrun) {
+        correctSound();
+        increaseScore();
+        makeBubble();
+        getNewHit();
+      } else {
+        wrongSound();
+      }
     }
-});
+  });
